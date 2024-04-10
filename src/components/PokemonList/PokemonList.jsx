2728adebb46a016aad5cd4fce3ab8a333a9b2f5a
Ankush2107 <ankush.kumar210701@gmail.com>
2024-04-10 13:40:01 +0530

    started some routing in every pokemon and the logo

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -15,29 +15,29 @@ const PokemonList = () => {
     async function downloadPokemons() {
         setIsLoading(true);
         const response = await axios.get(pokedexUrl); // This downloads list of 20 pokemon. 
-        console.log(response)
+        // console.log(response)
 
         const pokemonResults = response.data.results; // we get the array of pokemons from result
-        console.log(response.data);
+        // console.log(response.data);
         setNextUrl(response.data.next);
         setPrevUrl(response.data.previous);
-        console.log(pokemonResults);
+        // console.log(pokemonResults);
 
         /*
             iterating over the array of pokemons, and using their url, to create an array of promises
             that will download those 20 pokemons.  
         */
         const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url))
-        console.log(pokemonResultPromise)
+        // console.log(pokemonResultPromise)
 
         // passing that promise arrayto axios.all
         const pokemonData = await axios.all(pokemonResultPromise) // array of 20 pokemon detailed data
-        console.log(pokemonData);
+        // console.log(pokemonData);
 
         // now iterate on the data of each pokemon, and extract id, name, image and types.
         const pokeListResult = pokemonData.map((pokeData) => {
             const pokemon = pokeData.data;
-            console.log(pokemon);
+            // console.log(pokemon);
             return {
                 id: pokemon.id,
                 name: pokemon.name, 
@@ -46,7 +46,7 @@ const PokemonList = () => {
             }
         });
 
-        console.log(pokeListResult);
+        // console.log(pokeListResult);
         setPokemonList(pokeListResult);
         setIsLoading(false);
     }
